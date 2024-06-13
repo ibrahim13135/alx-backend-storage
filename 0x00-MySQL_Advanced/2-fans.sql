f266d711558af80436df56ece4335f50058e035f
Hima <abrahimmahmoud5454@gmail.com>
2024-06-13 18:55:31 +0300

    add bands

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,15 @@
+-- Write SQL script that ranks country origins of bands
+-- Ordered by the number of (non-unique) fans
+-- Column must be origin and nb_fans
+-- Script can be executed on any database
+-- 2-fans.sql
+
+SELECT 
+  origin, 
+  COUNT(*) AS nb_bands
+FROM 
+  metal_bands
+GROUP BY 
+  origin
+ORDER BY 
+  nb_bands DESC;

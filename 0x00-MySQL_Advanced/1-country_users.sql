4d99334873346430e3bdd8413a6386b469310843
Hima <abrahimmahmoud5454@gmail.com>
2024-06-13 18:15:24 +0300

    add country1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
---Create table users with unique Country constraint
+-- Use `IF NOT EXISTS` to prevent errors if the table already exists
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+    id INT AUTO_INCREMENT PRIMARY KEY,         -- Define id as integer, auto incrementing, and primary key
+    email VARCHAR(255) NOT NULL UNIQUE,       -- Define email as varchar(255), not null, and unique
+    name VARCHAR(255),                        -- Define name as varchar(255)
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'  -- Define country as an ENUM with specific values, not null, and defaulting to 'US'
 );

5a1d632b5061d0e5855711f8b6aef981ecc98a03
Hima <abrahimmahmoud5454@gmail.com>
2024-06-19 03:16:44 +0300

    task 14 avg database

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from pymongo import MongoClient
+from bson.objectid import ObjectId  # Import ObjectId for converting ObjectId to string
 
 def top_students(mongo_collection):
     pipeline = [
@@ -9,13 +10,32 @@ def top_students(mongo_collection):
         },
         {
             "$group": {
-                "_id": "$name",
+                "_id": "$_id",
+                "name": { "$first": "$name" },
                 "averageScore": { "$avg": "$topics.score" }
             }
         },
         {
             "$sort": { "averageScore": -1 }
+        },
+        {
+            "$project": {
+                "_id": { "$toString": "$_id" },
+                "formattedOutput": {
+                    "$concat": [
+                        "[",
+                        { "$toString": "$_id" },
+                        "] ",
+                        "$name",
+                        " => ",
+                        { "$toString": { "$round": ["$averageScore", 2] } }
+                    ]
+                }
+            }
         }
     ]
     
-    return list(mongo_collection.aggregate(pipeline))
+    cursor = mongo_collection.aggregate(pipeline)
+    formatted_results = [document['formattedOutput'] for document in cursor]
+    
+    return formatted_results

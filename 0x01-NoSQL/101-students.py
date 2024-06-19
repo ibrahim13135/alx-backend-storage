c3149019a76d74fa7afd1103fdbaa47624539e68
Hima <abrahimmahmoud5454@gmail.com>
2024-06-19 03:08:14 +0300

    task 14 avg database

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,13 +1,19 @@
+from pymongo import MongoClient
+
 def top_students(mongo_collection):
     pipeline = [
         {
-            "$project": {
-                "name": 1,
-                "topics": 1,
+            "$unwind": "$topics"
+        },
+        {
+            "$group": {
+                "_id": "$name",
                 "averageScore": { "$avg": "$topics.score" }
             }
         },
-        { "$sort": { "averageScore": -1 } }
+        {
+            "$sort": { "averageScore": -1 }
+        }
     ]
-
+    
     return list(mongo_collection.aggregate(pipeline))

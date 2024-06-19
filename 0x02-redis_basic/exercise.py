6fbc2939f42ff162f1e93c71b79640fbece7ab50
Hima <abrahimmahmoud5454@gmail.com>
2024-06-20 02:58:50 +0300

    task 1 set and cache

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+This module contains the Cache class which provides a simple interface
+to store data in Redis.
+"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """
+    Cache class for storing data in Redis with random keys.
+    """
+
+    def __init__(self):
+        """
+        Initialize the Cache instance.
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Store the given data in Redis with a random key.
+
+        Args:
+            data (Union[str, bytes, int, float]): The data to be stored.
+
+        Returns:
+            str: The key under which the data is stored.
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key

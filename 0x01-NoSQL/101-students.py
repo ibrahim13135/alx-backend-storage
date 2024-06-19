bfac6a6ec01058e8601e6ac662236fa701d54cae
Hima <abrahimmahmoud5454@gmail.com>
2024-06-19 03:17:31 +0300

    task 14 avg database

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,41 +1,19 @@
 #!/usr/bin/env python3
+"""
+Top students
+"""
 
-from pymongo import MongoClient
-from bson.objectid import ObjectId  # Import ObjectId for converting ObjectId to string
 
 def top_students(mongo_collection):
-    pipeline = [
-        {
-            "$unwind": "$topics"
-        },
-        {
-            "$group": {
-                "_id": "$_id",
-                "name": { "$first": "$name" },
-                "averageScore": { "$avg": "$topics.score" }
-            }
-        },
-        {
-            "$sort": { "averageScore": -1 }
-        },
-        {
-            "$project": {
-                "_id": { "$toString": "$_id" },
-                "formattedOutput": {
-                    "$concat": [
-                        "[",
-                        { "$toString": "$_id" },
-                        "] ",
-                        "$name",
-                        " => ",
-                        { "$toString": { "$round": ["$averageScore", 2] } }
-                    ]
-                }
-            }
-        }
-    ]
-    
-    cursor = mongo_collection.aggregate(pipeline)
-    formatted_results = [document['formattedOutput'] for document in cursor]
-    
-    return formatted_results
+    """
+    returns all students sorted by average score
+    :param mongo_collection:
+    :return:
+    """
+    return mongo_collection.aggregate([
+        {"$project": {
+            "name": "$name",
+            "averageScore": {"$avg": "$topics.score"}
+        }},
+        {"$sort": {"averageScore": -1}}
+    ])
